c741b40d109efdf538d2ce756c61c1da772e482c
tulioperes <88987070+tulioperes@users.noreply.github.com>
2021-09-30 11:17:53 -0300

    Add files via upload

---
+++ Programação-imperativa/AULA04/script.js
@@ -0,0 +1,13 @@
+// Comentario de uma unica linha.
+
+/*
+ Comentario de múltiplas linhas.
+  Utilizada principalmente em documentações de código.
+  */
+
+  var saudacao = "Olá"; 
+  var nome = 'Túlio';
+
+var nome = prompt(Qual o seu nome';
+
+  alert(saudacao + ' ' + nome);
---
+++ Programação-imperativa/AULA05/script.js
@@ -0,0 +1,41 @@
+/*
+    Matemáticos (valores aritmeticos para números)
+    + mais
+    - menos
+    * vezes
+    / divido
+    % resto da divisão
+    ** potência
+
+    Lógicos - responder verdeiro (true) ou falso (false)
+    < maior
+menor
+    <= menor ou igual
+    >= maior ou igual
+    == é igual?
+    === é igual mesmo? 
+    !=  é diferente de?
+
+    // Condicionais (ternários)
+    [condição] ? [casa seja verdadeiro] : [caso seja falso]
+
+    Exemplo
+    4> 6 ? 'É maior' : 'É menor
+
+*/
+
+
+// Função com parametro
+// Cada função tem um objetivo
+function saudar(mensagem,nomeDoUsuario) {
+    alert(mensagem + ' ' + nomeDoUsuario);
+}
+
+// 2. Métodos
+saudar(1235);
+saudar('Ola');
+saudar('oi');
+saudar('Seja Bem-Vindo');
+
+
+// Essa função faz... utiliza parametro para enviar saudação.
